cc7fece100ec292905223daa44e05b76ef10903f
Kharlotin <168816495+Kharlotin@users.noreply.github.com>
2024-05-03 12:19:58 +0300

    Add files via upload

---
+++ vesna.py
@@ -0,0 +1,12 @@
+n = int(input("номер мес€ца"))
+if n>0 and n<13:
+    if n<3 or n==12:
+        print("Winter")
+    elif n>=3 or n<6:
+        print("Vesna")
+    elif n>=6 or n<9:
+        print("Leto")
+    elif n>3 or n<6:
+        print("Osen")
+else:
+    print("error")
